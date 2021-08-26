3b8e224d88ab50edbfdf9b613311e64134554d86
c212 <40395648+c212@users.noreply.github.com>
2021-08-26 12:43:32 -0400

    created a timer

--- examples/timer/One.java
+++ examples/timer/One.java
@@ -1,5 +1,9 @@
+import javax.swing.Timer; 
+
 public class One {
   public static void main(String[] args) {
-
+    // https://docs.oracle.com/javase/tutorial/uiswing/misc/timer.html
+    Timer timer = new Timer(1000, null);
+    System.out.println( timer );      
   }
 }
