26aa6cdb4d386ee089ea95934d1948ff5653e437
c212 <40395648+c212@users.noreply.github.com>
2021-08-26 18:06:35 -0400

    timer reports every second

--- examples/second-lab/Nine.java
+++ examples/second-lab/Nine.java
@@ -1,9 +1,19 @@
-import javax.swing.Timer; 
+import javax.swing.Timer;
+import java.awt.event.ActionListener; 
+import java.awt.event.ActionEvent; 
+import javax.swing.JFrame;
 
-public class Nine {
+public class Nine implements ActionListener {
+  public void actionPerformed(ActionEvent e) {
+    System.out.println("Ouch!"); 
+  }
   public static void main(String[] args) {
+    JFrame a = new JFrame(); 
+    a.setSize(300, 200); 
+    a.setVisible(true); 
     // https://docs.oracle.com/javase/tutorial/uiswing/misc/timer.html
-    Timer timer = new Timer(1000, null);     
+    Timer timer = new Timer(1000, new Nine());     
     System.out.println( timer ); 
+    timer.start();
   } 
 }
