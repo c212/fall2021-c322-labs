bc40b5ebd73e423758ede8e89f96cce21cf9c481
c212 <40395648+c212@users.noreply.github.com>
2021-08-26 17:55:56 -0400

    creating and printing a timer

---
+++ examples/second-lab/Nine.java
@@ -0,0 +1,9 @@
+import javax.swing.Timer; 
+
+public class Nine {
+  public static void main(String[] args) {
+    // https://docs.oracle.com/javase/tutorial/uiswing/misc/timer.html
+    Timer timer = new Timer(1000, null);     
+    System.out.println( timer ); 
+  } 
+}
